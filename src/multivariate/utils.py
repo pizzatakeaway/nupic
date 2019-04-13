454577a67f43e585fe72892df5f078653d3fdd4d
pizzatakeaway <pizzatakeaway@mailbox.org>
2019-04-13 14:24:42 +0200

    requirements updated

--- src/multivariate/utils.py
+++ src/multivariate/utils.py
@@ -29,3 +29,32 @@ def computeRawAnomalyScore(activeColumns, prevPredictedColumns):
   else:
     # There are no active columns.
     score = 0.0
+
+def idx_active_cols(inputArr):
+    """
+    This function takes an 1d or nd-array and returns a 1d-array with the index for ACTIVE bits/columns: 
+    
+    Parameters
+    ----------
+    inputArr:   np.array (1d or nD)
+            
+    Output
+    ------
+    tmActiveColsIdx: 1d np.arraz
+        Array with index of active cols.
+        
+    """
+    #tmObject.reshape(tmObject.numberOfCols, tm.cellsPerColumn)
+    activeColsVec = []  # initialize vector
+
+    for i in range(inputArr.shape[0]):
+        # assign 1 if any 1 (active cell) in the column,
+        # 0 otherwise
+        if np.any(inputArr[i]>0):
+        # if np.any(tm.compute(spSDR[track[3]['sp_active']], enableLearn=True, enableInference=True).reshape(256, 3)[i]>0):
+            activeColsVec.append(1)
+        else:
+            activeColsVec.append(0)
+    # return index of active Columns        
+    tmActiveColsIdx = np.flatnonzero(np.array(activeColsVec))
+    return tmActiveColsIdx
